b427db8b6d3b74c187c2f49bd2662196abf6df50
cdfoye <cdfoye@gmail.com>
2022-07-28 16:21:30 -0600

    added Post to index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,8 @@
-const User = require('./User');
-const Product = require('./Product');
-const Category = require('./Category');
-const Order = require('./Order');
+const User = require("./User");
+const Product = require("./Product");
+const Category = require("./Category");
+const Order = require("./Order");
+// Post added by catherine 7/28
+const Post = require("./Post");
 
-module.exports = { User, Product, Category, Order };
+module.exports = { User, Product, Category, Order, Post };
