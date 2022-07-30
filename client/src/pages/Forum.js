c0ce1a15239a02240a5b403d08975c897b440d60
cdfoye <cdfoye@gmail.com>
2022-07-30 09:55:55 -0600

    added Forum.js to pages

---
+++ client/src/pages/Forum.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+
+import PostList from "../components/PostList";
+import PostForm from "../components/PostForm";
+
+import { QUERY_POSTS } from "../utils/queries";
+
+const Forum = () => {
+  const { loading, data } = useQuery(QUERY_POSTS);
+  const posts = data?.posts || [];
+
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: "1px dotted #1a1a1a" }}
+        >
+          <PostForm />
+        </div>
+        <div className="col-12 col-md-8 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <PostList thoughts={posts} title="Some Feed for Thought(s)..." />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Forum;
