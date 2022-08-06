7613e345d57f41896900e6742a5bcef9f5cb1abf
Aubree Zarges <aubreezarges@yahoo.com>
2022-08-05 18:43:59 -0600

    final aboutUs

--- client/src/components/AboutUs/AboutUsCard.js
+++ client/src/components/AboutUs/AboutUsCard.js
@@ -18,9 +18,9 @@ export default function AboutUsCard(props) {
         alt="Card cap"
       />
       <div className="card-body">
-        <h5 className="card-title">Name: {props.name}</h5>
-        <p className="card-text">Description: {props.description}</p>
-        <a href={props.GitHub} className="btn btn-primary">
+        <h5 className="card-title-Aubree">Name: {props.name}</h5>
+        <p className="card-text-Aubree">Description: {props.description}</p>
+        <a href={props.GitHub} className="AubreeLink" id="GitHubText">
           GitHub
         </a>
 
--- client/src/components/AboutUs/AboutUsDisplay.js
+++ client/src/components/AboutUs/AboutUsDisplay.js
@@ -2,7 +2,7 @@ import "./AboutUsStyle.css";
 import React from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/swiper.min.css';
-import 'swiper/components/navigation/navigation.min.css';
+// import 'swiper/components/navigation/navigation.min.css';
 
 
 
@@ -13,58 +13,48 @@ import AboutUsCard from './AboutUsCard';
 const data = [
   {
     name: 'Catherine',
-    description: 'Aspiring web developer interested in gaining a new skillset.',
+    description: 'I am a denver-based developer originally from the midwest. I wanted to get into coding professionally in order to challenge myself and potentially explore a new career.',
     GitHub: "https://github.com/cdfoye",
   },
 
   {
     name: 'Kelly',
-    description: 'Kelly is an inspiring front end developer located out of Colorado Springs',
+    description: 'I am a Colorado Springs based developer. I am ready to start a new career and am excited to learn as well as to bring a fresh perspective.                                                    ',
     GitHub: "https://github.com/kemwalsh",
   },
 
   {
     name: 'Aubree',
-    description: 'nurse turned developer',
+    description: 'Hello there! I have been a nurse for 9 years and am ready to start a new career with some fun challenges.                                                        ',
     GitHub: "https://github.com/AubreeZ-G",
   },
 
   {
     name: 'Cynthia',
-    description: 'From Interior Designer to Software Developer. Educated in full-stack development from the University of Denver.',
+    description: 'From Interior Designer to Software Developer. I am educated in full-stack development from the University of Denver.                                                          ',
     GitHub: "https://github.com/CynthiaGodoy",
   },
 ]
 export default function Display() {
   return (
+    <div className="Aubreewrapper">
+    <header className="Aubreeheader" id ="AubreePinkHeader">About Us</header>
+    <aside className="Aubreesidebar" id ="AubreePurpleSidebar">Hi There! Welcome to our app Toystalgia! We are an all women based group of full stack developers just graduated from Denver University's Coding Bootcamp. Together we wanted to make an app that brought joy with equal doses wimsy and nostalgia. So enjoy shopping for your favorite vintage toys and have fun! </aside>
+    <article className="Aubreecontent">
+      
+      
+      <div className="AboutUsDisplay"> <Swiper style={{
+            width: '18rem',
+          }} navigation={true} modules={[Navigation]} className="mySwiper">
+            {data.map(person => <SwiperSlide > <AboutUsCard name={person.name} description={person.description} GitHub={person.GitHub} /></SwiperSlide>)}
+  
+          </Swiper></div>
+    </article>
+    <footer className="Aubreefooter" id ="AubreeOrangeFooter"> </footer>
+  </div>
 
-    <div class="Aubreewrapper">
-      <header class="Aubreeheader">My header</header>
-      <aside class="Aubreesidebar">Sidebar</aside>
-      <article class="Aubreecontent">
-        <div className="AboutUsDisplay"> <Swiper style={{
-          width: '18rem',
-        }} navigation={true} modules={[Navigation]} className="mySwiper">
-          {data.map(person => <SwiperSlide > <AboutUsCard name={person.name} description={person.description} GitHub={person.GitHub} /></SwiperSlide>)}
-
-        </Swiper></div>
-
-      </article>
-      <footer class="Aubreefooter">My footer</footer>
-    </div>
 
   );
 
 }
 
-{
-  <div class="Aubreewrapper">
-  <header class="Aubreeheader">My header</header>
-  <aside class="Aubreesidebar">Sidebar</aside>
-  <article class="Aubreecontent">
-    <h1>2 column, header and footer</h1>
-    <p>This example uses line-based positioning, to position the header and footer, stretching them across the grid.</p>
-  </article>
-  <footer class="Aubreefooter">My footer</footer>
-</div>
-}
